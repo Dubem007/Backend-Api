87b13ee049cf03b98a8359221acbfc3680ae6240
Dubem Otekeiwebia <Dondubbie007@gmail.com>
2024-01-28 16:53:46 +0100

    "new project setup"

---
+++ src/controllers/users.ts
@@ -0,0 +1,35 @@
+import express from 'express'
+
+import {getUsers,deleteUserById} from '../db/users'
+
+export const getAllUsers = async (req: express.Request, res: express.Response) =>
+{
+    try{
+
+        const allUsers = await getUsers()
+
+        return res.status(200).json(allUsers)
+    }catch(error){
+      console.log(error)
+      return res.sendStatus(400)
+    }
+
+
+}
+
+export const deleteUser = async (req: express.Request, res: express.Response) =>
+{
+    try{
+
+        const { id } = req.params
+
+        const deletedUser = await deleteUserById(id)
+        
+        return res.status(200).json(deletedUser)
+    }catch(error){
+      console.log(error)
+      return res.sendStatus(400)
+    }
+
+}
+
---
+++ src/middleware/index.ts
@@ -0,0 +1,55 @@
+import express  from "express";
+import {get, merge} from "lodash"
+
+import { getUserBySessionToken } from "../db/users";
+
+export const isOwner = async( req: express.Request, res: express.Response, next: express.NextFunction) =>{
+
+    try{
+         const { id} = req.params
+
+         const currentUserId = await get(req, 'identity._id') as string
+         console.log("The currentUserId",JSON.stringify(currentUserId))
+         if(!currentUserId)
+         {
+            return res.sendStatus(403)
+         }
+
+         if(currentUserId === id)
+         {
+            console.log("The currentUserId is not same with id")
+            return res.sendStatus(403)
+         }
+
+         return next()
+    }catch(error){
+      console.log(error)
+      return res.sendStatus(403)
+    }
+}
+
+
+export const isAuthenticated = async( req: express.Request, res: express.Response, next: express.NextFunction) =>{
+
+    try{
+         const sessionToken = req.cookies['DUBEM-AUTH']
+
+         if(!sessionToken)
+         {
+            return res.sendStatus(403)
+         }
+
+         const existingUser = await getUserBySessionToken(sessionToken)
+         if(!existingUser)
+         {
+            return res.sendStatus(403)
+         }
+
+         merge(req, {identity:existingUser })
+
+         return next()
+    }catch(error){
+      console.log(error)
+      return res.sendStatus(403)
+    }
+}
